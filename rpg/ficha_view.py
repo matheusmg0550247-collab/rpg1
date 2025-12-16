a7725f89414be3652cac5f2ea1756c0d8261fe58
matheusmg0550247-collab <matheusmg0550247@gmail.com>
2025-12-16 17:39:00 -0300

    Update ficha_view.py

--- rpg/ficha_view.py
+++ rpg/ficha_view.py
@@ -3,7 +3,7 @@ import streamlit as st
 import uuid
 from pathlib import Path
 
-from rpg.storage import ensure_dirs, list_character_ids, load_character, save_character, PORTRAIT_DIR
+from rpg.storage import ensure_dirs, list_character_ids, load_character, save_character, PORTRAIT_DIR, delete_character
 from rpg.pdf_import import import_character_from_pdf
 from rpg.utils import ability_mod, proficiency_bonus
 from rpg.dice import roll_d20, roll_expr, critify, fmt_d20, fmt_expr
